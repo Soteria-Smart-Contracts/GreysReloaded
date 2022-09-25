c7f208bf13c51e6712ac035e5fb75c5a929d6216
Soteria Smart Contracts <62032531+Soteria-Smart-Contracts@users.noreply.github.com>
2022-09-25 12:02:03 -0400

    Add files via upload

---
+++ src/Functionality.js
@@ -0,0 +1,108 @@
+SwitchPage("mint");
+let CollectionNum = 501;
+let updown;
+
+document.getElementById("txviewer").style.display = "none";
+
+function setUpdown(direction){
+    document.getElementById("CollectionContainer").innerHTML = "";
+    if (direction == "up"){
+        CollectionNum = 1;
+        updown = "up";
+    }
+    if (direction == "down"){
+        CollectionNum = 10000;
+        updown = "down";
+    }
+    ShowCollection();
+}
+
+function ShowCollection(){
+    let index = CollectionNum;
+
+    if (updown == "down"){
+        while(index > (CollectionNum - 100)){
+            if (index < 1 || index > 501){
+                break;
+            }
+            let indexid = index;
+            newcard = document.createElement("div");
+            newcard.className = "NFTcard";
+            newcard.id = indexid;
+            let img = '<img class="nftimage" src="src/images/collection/' + indexid + '.PNG"></img>"';
+            let BlockscoutLink = 'https://blockscout.com/etc/mainnet/token/0x2001d679210c0e4531f5c07155d8e3677dd388ae/instance/' + indexid +  '/token-transfers';
+            newcard.innerHTML = img + '<a class="nftID head">GreysReloaded ID:</a>' + '<br>' + '<a class="nftID">' + indexid + '</a>' + '<br>' + '<a class="nftID scout" href="' + BlockscoutLink + '" target="_blank">View On Blockscout</a>';
+            document.getElementById("CollectionContainer").appendChild(newcard);
+            index--;
+        }
+    }
+
+    if (updown == "up"){
+        while(index < (CollectionNum + 100)){
+            if (index < 1 || index > 501){
+                break;
+            }
+            let indexid = index;
+            newcard = document.createElement("div");
+            newcard.className = "NFTcard";
+            newcard.id = indexid;
+            let img = '<img class="nftimage" src="src/images/collection/' + indexid + '.PNG"></img>"';
+            let BlockscoutLink = 'https://blockscout.com/etc/mainnet/token/0x2001d679210c0e4531f5c07155d8e3677dd388ae/instance/' + indexid +  '/token-transfers';
+            newcard.innerHTML = img + '<a class="nftID head">GreysReloaded ID:</a>' + '<br>' + '<a class="nftID">' + indexid + '</a>' + '<br>' + '<a class="nftID scout" href="' + BlockscoutLink + '" target="_blank">View On Blockscout</a>';
+            document.getElementById("CollectionContainer").appendChild(newcard);
+            index++;
+        }
+    }
+
+    CollectionNum = index;
+}
+
+
+function SwitchPage(page) {
+    document.getElementById("txviewer").style.display = "none";
+    if(page == "mint"){
+        document.getElementById("wallet").style.display = "none";
+        document.getElementById("collection").style.display = "none";
+        document.getElementById("minting").style.display = "block";
+    }
+    if(page == "wallet"){
+        document.getElementById("minting").style.display = "none";
+        document.getElementById("collection").style.display = "none";
+        document.getElementById("wallet").style.display = "block";
+    }
+    if(page == "collection"){
+        document.getElementById("minting").style.display = "none";
+        document.getElementById("wallet").style.display = "none";
+        document.getElementById("collection").style.display = "block";
+    }
+
+}
+
+function GetRank(id){
+    let index = 0;
+    while(index < 10000){
+        if (rankingorder[index] == id){
+            return index + 1;
+        }
+        index++;
+    }
+}
+
+
+function LoadNFTs() {
+    let index = 0;
+    while(index < ids.length){
+        newcard = document.createElement("div");
+        id = ids[index];
+        rank = GetRank(id);
+        newcard.className = "NFTcard";
+        newcard.id = index;
+        let img = '<img class="nftimage" src="src/images/collection/' + id + '.PNG"></img>"';
+        let BlockscoutLink = 'https://blockscout.com/etc/mainnet/token/0x18265d64393Ca917995Abf34bf73d39952f2D85b/instance/' + ids[index] +  '/token-transfers';
+        newcard.innerHTML = img + '<a class="nftID head">GreysReloaded ID:</a>' + '<br>' + '<a class="nftID">' + id + '</a>' + '<br>' + '<a class="nftID scout" href="' + BlockscoutLink + '" target="_blank">View On Blockscout</a>';
+        document.getElementById("WalletContainer").appendChild(newcard);
+        index++;
+    }
+
+}
+
---
+++ src/Web3.js
@@ -0,0 +1,77 @@
+let account;
+const ABI = window.abi;
+let ids;
+let netID;
+const contractAddress = "0x18265d64393Ca917995Abf34bf73d39952f2D85b";
+
+loginWithEth();
+
+
+async function loginWithEth(){
+    if(window.ethereum){
+        await ethereum.request({ method: 'eth_requestAccounts' });
+        window.web3 = await new Web3(ethereum);
+        await getID();
+        if (netID != 61){
+            console.log("The current Metamask/Web3 network is not Ethereum Classic, please connect to the Ropsten test network."); //CHANGE FOR REAL CROWDSALE TO ETC
+            alert("The current Metamask/Web3 network is not ETC, please connect to the Ethereum Classic test network.");
+            showOverlay();
+            return("Failed to connect")
+        }
+        accountarray = await web3.eth.getAccounts();
+        contract = await new window.web3.eth.Contract(ABI, contractAddress, window.web3);
+        account = accountarray[0];
+        getsupply();
+        await ReturnIds();
+        LoadNFTs();
+    } else { 
+        alert("No ETHER Wallet available")
+    }
+}
+
+async function getsupply(){
+    supply = await contract.methods.totalSupply().call();
+    document.getElementById("supply").innerHTML = supply;
+}
+
+async function getID(){
+    let idhex = web3.eth._provider.chainId;
+    netID = parseInt(idhex, 16);
+
+    return(netID);
+}
+
+async function mint(){
+    let amount = document.getElementById("amount").value;
+    let txfailed = false;
+    document.getElementById("txviewer").innerHTML = 'Awaiting Blockchain confirmation <br><img src="src/images/spin.gif" id="spin" class="spingif"></img>';
+    document.getElementById("txviewer").target = "none";
+    document.getElementById("txviewer").href = "";
+    if(amount < 1){ 
+        alert("Please enter a valid amount");
+        return;
+    }
+    let amountWei = amount * 650000000000000000;
+    document.getElementById("txviewer").style.display = "block";
+    let tx = await contract.methods.mint(amount).send({from: account, value: amountWei, gas: 3000000}).catch(err => {
+        console.log("Transaction failed");
+        alert("Failed to mint");
+        document.getElementById("txviewer").style.display = "none";
+        txfailed = true;
+        return;
+    });
+    if(txfailed == true){
+        return;
+    }
+    document.getElementById("txviewer").textContent = "Transaction Complete";
+    document.getElementById("txviewer").target = "_blank";
+    document.getElementById("txviewer").href = "https://blockscout.com/etc/mainnet/tx/" + tx.transactionHash;
+    console.log(tx);
+    return(tx);
+}
+
+async function ReturnIds(){
+    ids = await contract.methods.walletOfOwner(account).call();
+    console.log(ids);
+    return(ids);
+}
